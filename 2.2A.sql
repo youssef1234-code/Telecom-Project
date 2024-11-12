cd5c1fdf4cf4dbf004a3cc719bc40913c7346f9d
Yassin <yassinmohamed007@gmail.com>
2024-11-12 15:34:48 +0200

    I love Dr Ahmed El Mahdy - yassin push!!!

---
+++ 2.2A.sql
@@ -0,0 +1,33 @@
+﻿use Telecom_Team_1
+GO
+CREATE VIEW allServicePlans
+	AS
+	SELECT *
+	FROM Service_Plan
+	;
+GO
+CREATE VIEW AccountPayments
+	AS
+	SELECT p.amount, p.date_of_payment, cp.first_name, cp.lASt_name
+	FROM Payment p, customer_account ca, customer_profile cp
+	WHERE p.mobileNo = ca.mobileNo AND cp.nationalID = ca.nationalID
+GO
+CREATE VIEW allResolvedTickets
+AS
+	SELECT *
+	FROM Technical_Support_Ticket
+	WHERE status = 'Resolved'
+GO
+CREATE VIEW E_shopVouchers
+AS
+	SELECT e.URL, v.value, v.voucherID
+	FROM E_shop e, Voucher v
+	WHERE e.shopID = v.shopID AND v.redeem_date IS NOT NULL
+GO
+CREATE VIEW Num_of_cAShback
+AS
+	SELECT w.walletID,Count(c.CAShbackID) AS Number_Of_CAShbacks
+	FROM Wallet w, CAShback c
+	WHERE w.walletID= c.walletID
+	Group By w.walletID
+GO
---
+++ 2.4A.sql
@@ -0,0 +1,59 @@
+﻿use Telecom_Team_1
+GO
+CREATE FUNCTION AccountLoginValidation
+(@MobileNo char(11),@Password varchar(50))
+RETURNS BIT
+AS
+BEGIN
+DECLARE @Success BIT
+	if exists (SELECT * FROM Customer_Account c WHERE c.mobileNo = @MobileNo AND c.pass = @Password)
+	Set @Success =1
+	else set @Success =0;
+RETURN @Success
+END
+GO
+CREATE FUNCTION Unsubscribed_Plans
+(@MobileNo char(11))
+RETURNS TABLE
+AS
+RETURN 
+(SELECT s.name
+FROM Service_Plan s, Subscription sub
+WHERE s.planID <> sub.PlanID AND sub.mobileNo = @MobileNo
+)
+GO
+CREATE FUNCTION Cashback_Wallet_Customer
+(@NationalID int)
+RETURNS TABLE
+AS
+RETURN
+(
+SELECT c.*
+FROM Cashback c, Wallet w
+WHERE c.WalletID = w.WalletID AND w.NationalID = @NationalID
+)
+GO
+CREATE FUNCTION Account_Highest_Voucher
+(@MobileNo car(11))
+RETURNS INT
+AS 
+BEGIN
+DECLARE @Voucher_id INT
+SELECT MAX(v.voucherID) as maximum
+FROM Voucher v
+WHERE v.mobileNo = @MobileNo
+set @Voucher_id = maximum
+RETURN @Voucher_id
+END
+GO
+CREATE FUNCTION Subscribed_Plans_5_Months
+(@MobileNo char(11))
+RETURNS TABLE
+AS
+RETURN
+(
+SELECT s.*
+FROM Service_Plan s, Subscription sub
+WHERE s.planID = sub.planID AND sub.mobileNo = @MobileNo AND DATEDIFF(MONTH,sub.subscription_date,GETDATE()) < 5
+)
+GO
